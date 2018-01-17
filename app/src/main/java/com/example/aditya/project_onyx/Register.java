c64ce23a899b95b6053c4da53296cb9c163ec46d
uncoded-decimal <30970288+uncoded-decimal@users.noreply.github.com>
2018-01-18 03:32:24 +0530

    chal gaya

--- app/src/main/java/com/example/aditya/project_onyx/Register.java
+++ app/src/main/java/com/example/aditya/project_onyx/Register.java
@@ -4,26 +4,37 @@ package com.example.aditya.project_onyx;
  * Created by Anushka on 17-01-2018.
  */
 
-import java.net.URL;
-import java.net.URLEncoder;
-
-import android.app.Activity;
+import android.app.ProgressDialog;
+import android.net.http.RequestQueue;
 import android.os.Bundle;
-import android.os.StrictMode;
-import android.support.design.widget.TextInputEditText;
+import android.provider.SyncStateContract;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class Register extends Activity {
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Register extends AppCompatActivity {
 
     final String myTag = "DocsUpload";
-    EditText et1,et2,et3,et4;
-    TextView et5;
+    EditText et1,et2,et3,et4, et5;
     Button but;
+    ProgressDialog progressDialog;
+    com.android.volley.RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,54 +45,74 @@ public class Register extends Activity {
         et2=(EditText)findViewById(R.id.pEmail) ;
         et3=(EditText)findViewById(R.id.pCollege) ;
         et4=(EditText)findViewById(R.id.pPhone) ;
-        et5=(TextView) findViewById(R.id.pEvent) ;
+        et5=(EditText) findViewById(R.id.pEvent) ;
         but=(Button)findViewById(R.id.send) ;
+        queue = Volley.newRequestQueue(getApplicationContext());
         Log.i(myTag, "OnCreate()");
+        et5.setText(getIntent().getStringExtra("EventName"));
+        //et5.setEditableFactory();
 
-        Bundle b=getIntent().getExtras();
-        et5.setText(b.getString("title"));
-
-        final Thread t = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                postData();
-
-            }
-        });
-        t.start();
-
-
-    }
-
-    public void postData() {
-
-        final String fullUrl = "https://docs.google.com/forms/d/e/1FAIpQLSdt19FBT6aN2EBVYsItllFKLFfqL50qSxCVY_RZW4xUpsXcqg/formResponse";
-        final HttpRequest mReq = new HttpRequest();
-        final String name = et1.getText().toString();
-        final String email = et2.getText().toString();
-        final String college = et3.getText().toString();
-        final String phone = et4.getText().toString();
-        final String event = et5.getText().toString();
-        final String data = "entry_781202710=" + URLEncoder.encode(name) + "&" +
-                "entry_45181195=" + URLEncoder.encode(college) + "&" + "entry_71603580=" +
-                URLEncoder.encode(email)+ "&" + "entry_1895487815=" + URLEncoder.encode(phone) + "&" +
-                "entry_974094215=" + URLEncoder.encode(event);
-
+        progressDialog= new ProgressDialog(this);
+        progressDialog.setCancelable(false);
+        progressDialog.setMessage("Setting you Up...");
 
         but.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(name!=null && college!=null && phone!=null && email!=null && event!=null){
-                String response;
-                mReq.sendPost(fullUrl, data);
-                finish();
-                //Log.i(myTag, response);
-                }
-                else
-                    Toast.makeText(Register.this,"One Field left Empty", Toast.LENGTH_SHORT).show();
+                postData(et1.getText().toString(),et2.getText().toString(),et3.getText().toString()
+                ,et4.getText().toString(),et5.getText().toString());
             }
         });
+    }
 
+    public void postData(final String name, final String email, final String college ,final String phone, final String event) {
+
+        progressDialog.show();
+        final String fullUrl = "https://docs.google.com/forms/d/e/1FAIpQLSdt19FBT6aN2EBVYsItllFKLFfqL50qSxCVY_RZW4xUpsXcqg/formResponse";
+        StringRequest request = new StringRequest(
+                Request.Method.POST,
+                fullUrl,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("TAG", "Response: " + response);
+                        if (response.length() > 0) {
+                            Snackbar.make(but, "Successfully Posted", Snackbar.LENGTH_LONG).show();
+                            et1.setText(null);
+                            et2.setText(null);
+                            et3.setText(null);
+                            et4.setText(null);
+                            et5.setText(null);
+                            finish();
+                        } else {
+                            Snackbar.make(but, "Try Again", Snackbar.LENGTH_LONG).show();
+                        }
+                        progressDialog.dismiss();
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                progressDialog.dismiss();
+                Snackbar.make(but, "Error while Posting Data", Snackbar.LENGTH_LONG).show();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("entry.781202710", name);
+                params.put("entry.71603580", email);
+                params.put("entry.1895487815", phone);
+                params.put("entry_45181195", college);
+                params.put("entry_974094215",event);
+                return params;
+            }
+        };
+        request.setRetryPolicy(new DefaultRetryPolicy(
+                50000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        queue.add(request);
     }
 
 }
